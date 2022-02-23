ea7c3a3c459035009f09f95da9135607f04e7571
Shane Cotta <37381488+Shane-Cotta@users.noreply.github.com>
2022-02-22 16:08:27 -0800

    Updated to include logging features...

--- CSV/CSVProccessor.ps1
+++ CSV/CSVProccessor.ps1
@@ -1,8 +1,8 @@
 #Configurable Vars
 $FileName = "users.csv"
 $EmailDomain = "@abc.edu"
-$Source = "c:/temp/incoming/"
-$Destination = "c:/temp/outgoing/"
+$Source = "/Users/shanecotta/Desktop/"
+$Destination = "/Users/shanecotta/Desktop/"
 $TimeStamp = Get-Date -Format "yyyyMMddHHmmss"
 # Email Settings
 $MailSender = "no-reply@domain.com"
@@ -17,9 +17,19 @@ $EmailPassword = "password"
 ##################
 try {
     $Results = @{}
-    $SourceResults = @{}
+    $Delta = @{}
     $Path = $Source + $FileName
-    $proccessed = $Destination + "Proccessed"
+    $LogFile = $Source + "log.txt"
+    # Check if Directory Structure and script files exists
+    if (Test-Path -Path $Destination\"Proccessed"){
+    } else {
+        New-Item -ItemType directory $Destination\"Proccessed"
+    }
+    if ([System.IO.File]::Exists($LogFile)){
+    } else{
+        New-Item -ItemType "file" -Path $LogFile
+        Add-Content -Path $LogFile -Value "TIMESTAMP, STATUS, MESSAGE"
+    }
     # Check if file exists
     if ([System.IO.File]::Exists($Path)){
         # Proccess each record within csv
@@ -28,45 +38,53 @@ try {
             if ($row.emailaddress -match $EmailDomain)
             {
                 # Add matching results to Hash table
-                $Results.add($row.name,$row.emailaddress)
+                $Results.add($row.emailaddress,$row.name)
+                $MSG = "$TimeStamp, INFO, Added " + $row.emailaddress
+                Add-Content -Path $LogFile -Value $MSG
             } else {
-                $SourceResults.add($row.name,$row.emailaddress)
-                Write-Host $row.emailaddress "Does not match, moving on..."
+                # Add Deltas to the delta hash table
+                $Delta.add($row.emailaddress,$row.name)
+                $MSG = "$TimeStamp, WARN, " + $row.emailaddress + " Does not match domain"
+                Add-Content -Path $LogFile -Value $MSG
             }
         }
         # Export proccessed Result set to timestamped csv
         $Results.GetEnumerator() |
-            Select-Object -Property @{N='name';E={$_.Key}},
-            @{N='emailaddress';E={$_.Value}} | 
-            Export-Csv -UseQuotes AsNeeded -NoTypeInformation -Path $Destination\"users$TimeStamp.csv"
-        if (Test-Path -Path $proccessed){
-            Move-Item -Path $Source$FileName -Destination $proccessed\"$TimeStamp.csv"
-        }
-        else {
-            New-Item -ItemType directory $Destination\"Proccessed"
-        }
+            Select-Object -Property @{N='name';E={$_.Value}},
+            @{N='emailaddress';E={$_.Key}} | 
+            Export-Csv -UseQuotes AsNeeded -NoTypeInformation -Path $Destination\"Proccessed"\"users$TimeStamp.csv"
+            ##UNCOMMENT ME
+            ##Move-Item -Path $Source$FileName -Destination $proccessed\"$TimeStamp.csv"
         # Send follow up email.
         try {       
             $message = new-object Net.Mail.MailMessage;
             $message.From = $MailSender;
             $message.To.Add($MailReciepient);
             $message.Subject = "Emails have been proccessed";
-            $message.Body = $Results.Count + " Emails met criteria and were proccessed `n" + $SourceResults.Count + " Did not meet criteria `n" + "Job Finished:" + $TimeStamp;
-        
+            $Body = [string]$Results.Count + " Emails met criteria and were proccessed `n" + [string]$Delta.Count + " Did not meet criteria `n" + "Job Finished:" + [string]$TimeStamp
+            $message.Body = $Body;
             $smtp = new-object Net.Mail.SmtpClient($SMTPServer, $Port);
             $smtp.EnableSSL = true;
             $smtp.Credentials = New-Object System.Net.NetworkCredential($EmailUsername, $EmailPassword);
             $smtp.send($message);
         }
         catch {
+            $MSG = "$TimeStamp, ERROR, SMTP: " + $_.Exception.Message
+            Add-Content -Path $LogFile -Value $MSG
         }
+        Write-Host "########## Deltas"
+        Write-Host @Delta
+        Write-Host "########## Results"
+        Write-Host @Results
     # If user.csv is not found continue
     } else {
-        Write-Host "Nothing to proccess here..."
+        $MSG = "$TimeStamp, INFO, Nothing to proccess found"
+        Add-Content -Path $LogFile -Value $MSG
     }
 }
 #Log Errors
 catch {
     # You can also use this to error log to a Sql DB or call a webhook with a ticket system to create a bug (ex. Jira, Service Now and etc...)
-    Write-Error $_
+    $MSG = "$TimeStamp, ERROR, " + $_.Exception.Message
+    Add-Content -Path $LogFile -Value $MSG
 }
