04520239809b90b0ba92e1ed4e61dcc4029e34ff
Shane Cotta <37381488+Shane-Cotta@users.noreply.github.com>
2022-02-23 13:43:47 -0800

    Introduced task scheduling helper.

--- CSV/CSVProccessor.ps1
+++ CSV/CSVProccessor.ps1
@@ -1,26 +1,79 @@
-#Configurable Vars
-$FileName = "users.csv"
-$EmailDomain = "@abc.edu"
-$RootDIR = "/Users/shanecotta/Desktop/"
-$SourceFolder = "incoming"
-$DestinationFolder = "outgoing"
-$ProccessedFolder = "proccessed"
-$TimeStamp = Get-Date -Format "yyyyMMddHHmmss"
-# Email Settings
-$MailSender = "no-reply@keepitsimply.net"
-$MailReciepient = "me@shanecotta.com"
-$SMTPServer = "email-smtp.us-west-1.amazonaws.com"
-$Port = "587"
-$EmailUsername = "AKIAQTI42N62WR53QXKX"
-$EmailPassword = $args[0]
+# Determine if human or scheduled job is running script
+if ($args) {
+    $FileName = $args[0]
+    $EmailDomain = $args[1]
+    $RootDIR = $args[2]
+    # Email Settings
+    $MailSender = $args[3]
+    $MailReciepient = $args[4]
+    $SMTPServer = $args[5]
+    $Port = $args[6]
+    $EmailUsername = $args[7]
+    $EmailPassword = $args[8]
+} else {
+    # Ask the user for global vars needed for a manual execution
+    $Ask = Read-Host -Prompt 'Would you like to setup a scheduled task? (Y or N)'
+    $File = Read-Host -Prompt 'Input the expected file name to be read (ex. users.csv)'
+    $DomainMatch = Read-Host -Prompt 'Input the domains you would like matched from the csv (ex. @abc.edu)'
+    $Source = $PSScriptRoot + "/"
+    $EmailSender = Read-Host -Prompt 'Input the email sender (ex. no-reply@keepitsimply.net)'
+    $EmailReciepient = Read-Host -Prompt 'Input the email reciepient (ex. me@shanecotta.com)'
+    $EmailServer = Read-Host -Prompt 'Input the email server (ex. email-smtp.us-west-1.amazonaws.com)'
+    $EmailPort = Read-Host -Prompt 'Input the email server port (ex. 587)'
+    $EmailUser = Read-Host -Prompt 'Input the email Username (ex. AKIAQTI42N62WR53QXKX)'
+    $EmailPass = Read-Host -Prompt 'Input the email Password (ex. 1234)'
+    if ($Ask -eq "Y"){
+        # Create Scheduled Task in task manager and pass prior execution vars
+        $ScriptPath = $Source + "CSVProccessor.ps1 " + $File + " " + $DomainMatch  + " " + $Source + " " + $EmailSender + " " + $EmailReciepient + " " + $EmailServer + " " + $EmailPort + " " + $EmailUser + " " + $EmailPass
+        $ScheduleTime = Read-Host -Prompt 'Input the time of day you would like the job to run (ex. 9:15 AM)'
+        $RunAs = Read-Host -Prompt 'Input the user like the job to run as (ex. DOMAIN\user)'
+        $actions = (New-ScheduledTaskAction -Execute $ScriptPath)
+        $trigger = New-ScheduledTaskTrigger -Daily -At $ScheduleTime
+        $principal = New-ScheduledTaskPrincipal -UserId $RunAs -RunLevel Highest
+        $settings = New-ScheduledTaskSettingsSet -RunOnlyIfNetworkAvailable -WakeToRun
+        $task = New-ScheduledTask -Action $actions -Principal $principal -Trigger $trigger -Settings $settings
+        Register-ScheduledTask 'CSVProccessor' -InputObject $task
+        # Vars to use for init. execution
+        $FileName = $File
+        $EmailDomain = $DomainMatch
+        $RootDIR = $Source
+        # Email Settings
+        $MailSender = $EmailSender
+        $MailReciepient = $EmailReciepient
+        $SMTPServer = $EmailServer
+        $Port = $EmailPort
+        $EmailUsername = $EmailUser
+        $EmailPassword = $EmailPass
+    } elseif ($Ask -eq "N") {
+            # Vars to use for one time only manual execution
+            $FileName = $File
+            $EmailDomain = $DomainMatch
+            $RootDIR = $Source
+            # Email Settings
+            $MailSender = $EmailSender
+            $MailReciepient = $EmailReciepient
+            $SMTPServer = $EmailServer
+            $Port = $EmailPort
+            $EmailUsername = $EmailUser
+            $EmailPassword = $EmailPass
+    } else {
+        # Scold the user for not following directions :)
+        Write-Host "You need to Specify Y or N"
+    }
+}
 
 ##################
 # MAIN
 ##################
 try {
+    $SourceFolder = "incoming"
+    $DestinationFolder = "outgoing"
+    $ProccessedFolder = "proccessed"
+    $TimeStamp = Get-Date -Format "yyyyMMddHHmmss"
     $Results = @{}
     $Delta = @{}
-    $Path = $RootDIR + $FileName
+    $Path = $RootDIR + $SourceFolder + "/" + $FileName
+    Write-Host $Path
     $LogFile = $RootDIR + "log.txt"
     # Check if Directory Structure(s) and log file exists, if not create and start logging
     if ([System.IO.File]::Exists($LogFile)){
@@ -28,12 +81,14 @@ try {
         New-Item -ItemType "file" -Path $LogFile
         Add-Content -Path $LogFile -Value "TIMESTAMP,STATUS,MESSAGE"
         $MSG = "$TimeStamp,WARN,Logfile missing: created " + $LogFile
+        Write-Host $MSG
         Add-Content -Path $LogFile -Value $MSG
     }
     if (Test-Path -Path $RootDIR\$SourceFolder){
     } else {
         New-Item -ItemType directory $RootDIR\$SourceFolder
         $MSG = "$TimeStamp,WARN,Source DIR missing: created " + $RootDIR + $SourceFolder
+        Write-Host $MSG
         Add-Content -Path $LogFile -Value $MSG
     }
     if (Test-Path -Path $RootDIR\$DestinationFolder){
@@ -46,6 +101,7 @@ try {
     } else {
         New-Item -ItemType directory $RootDIR\$ProccessedFolder
         $MSG = "$TimeStamp,WARN,Proccessed DIR missing: created " + $RootDIR + $ProccessedFolder
+        Write-Host $MSG
         Add-Content -Path $LogFile -Value $MSG
     }
     # Check if dropped source file exists
@@ -58,11 +114,13 @@ try {
                 # Add matching results to Hash table
                 $Results.add($row.emailaddress,$row.name)
                 $MSG = "$TimeStamp,INFO,Added " + $row.emailaddress
+                Write-Host $MSG
                 Add-Content -Path $LogFile -Value $MSG
             } else {
                 # Add Deltas to the delta hash table
                 $Delta.add($row.emailaddress,$row.name)
                 $MSG = "$TimeStamp,WARN," + $row.emailaddress + " Does not match domain"
+                Write-Host $MSG
                 Add-Content -Path $LogFile -Value $MSG
             }
         }
@@ -70,9 +128,9 @@ try {
         $Results.GetEnumerator() |
             Select-Object -Property @{N='name';E={$_.Value}},
             @{N='emailaddress';E={$_.Key}} | 
-            Export-Csv -UseQuotes AsNeeded -NoTypeInformation -Path $RootDIR\$DestinationFolder\"users$TimeStamp.csv"
+            Export-Csv -NoTypeInformation -Path $RootDIR\$DestinationFolder\"users$TimeStamp.csv"
             ##UNCOMMENT ME
-            ##Move-Item -Path $RootDIR$FileName -Destination $RootDIR\$ProccessedFolder\"$TimeStamp.csv"
+            Move-Item -Path $RootDIR\$SourceFolder\$FileName -Destination $RootDIR\$ProccessedFolder\"$TimeStamp.csv"
         # Send follow up email.
         try {       
             $message = new-object Net.Mail.MailMessage;
@@ -88,16 +146,19 @@ try {
         }
         catch {
             $MSG = "$TimeStamp,ERROR,SMTP: " + $_.Exception.Message
+            Write-Host $MSG
             Add-Content -Path $LogFile -Value $MSG
         }
     # If user.csv is not found continue
     } else {
-        $MSG = "$TimeStamp,INFO,Nothing to proccess found"
+        $MSG = "$TimeStamp,INFO,The file $Path was not found to proccess"
+        Write-Host $MSG
         Add-Content -Path $LogFile -Value $MSG
     }
 }
 #Log Errors
 catch {
     $MSG = "$TimeStamp,ERROR," + $_.Exception.Message
+    Write-Host $MSG
     Add-Content -Path $LogFile -Value $MSG
 }
